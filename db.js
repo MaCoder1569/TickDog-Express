5f0e8727753e90bfde19946189c169694ced1827
WOOIL KIM <dndlf_93@torpedo.co.kr>
2023-03-06 16:53:48 +0900

    카카오 회원가입 및 로그인 기능 초기 구현

---
+++ db.js
@@ -0,0 +1,10 @@
+const neo4j = require("neo4j-driver");
+
+const uri = process.env.NEO4J_URI;
+const user = process.env.NEO4J_USER;
+const password = process.env.NEO4J_PASSWORD;
+
+const driver = neo4j.driver(uri, neo4j.auth.basic(user, password));
+const session = driver.session();
+
+module.exports = { driver, session };
--- server.js
+++ server.js
@@ -1,21 +1,82 @@
 require("dotenv").config();
 const express = require("express");
-const app = express();
-const port = process.env.PORT || 3000;
-const morgan = require("morgan");
-const cors = require("cors");
-const helmet = require("helmet");
+const axios = require("axios");
+const querystring = require("querystring");
+const neo4j = require("neo4j-driver");
 
-app.use(morgan("dev"));
-app.use(cors());
-app.use(helmet());
+const app = express();
+const port = process.env.PORT;
+
+const kakaoClientId = process.env.KAKAO_CLIENT_ID;
+const kakaoClientSecret = process.env.KAKAO_CLIENT_SECRET;
+const kakaoRedirectUri = process.env.KAKAO_REDIRECT_URI;
+
+const neo4jDriver = neo4j.driver(
+  process.env.NEO4J_URI,
+  neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASSWORD)
+);
 
 app.get("/", (req, res) => {
-  res.json({
-    success: true,
-  });
+  res.send("Hello, world!");
+});
+
+app.get("/auth/kakao", (req, res) => {
+  const kakaoAuthUrl =
+    "https://kauth.kakao.com/oauth/authorize?" +
+    querystring.stringify({
+      client_id: kakaoClientId,
+      redirect_uri: kakaoRedirectUri,
+      response_type: "code",
+      scope: "account_email,gender",
+    });
+  res.redirect(kakaoAuthUrl);
+});
+
+app.get("/auth/kakao/callback", async (req, res) => {
+  const code = req.query.code;
+  try {
+    const tokenResponse = await axios.post(
+      "https://kauth.kakao.com/oauth/token",
+      querystring.stringify({
+        grant_type: "authorization_code",
+        client_id: kakaoClientId,
+        client_secret: kakaoClientSecret,
+        redirect_uri: kakaoRedirectUri,
+        code,
+      }),
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }
+    );
+    const accessToken = tokenResponse.data.access_token;
+    const userResponse = await axios.get("https://kapi.kakao.com/v2/user/me", {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    const user = userResponse.data;
+    const session = neo4jDriver.session();
+    await session.run(
+      "MERGE (user:User {id: $id}) " +
+        "SET user.name = $name, user.email = $email " +
+        "RETURN user",
+      {
+        id: user.id,
+        name: user.properties.nickname,
+        email: user.kakao_account.email,
+        gender: user.kakao_account.gender,
+      }
+    );
+    session.close();
+    res.send(user);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
+  }
 });
 
 app.listen(port, () => {
-  console.log(`server is listening at localhost:${port}`);
+  console.log(`Server listening at http://localhost:${port}`);
 });
