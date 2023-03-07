31268590e5ad3901e6be7c3e647fe768688533cf
WOOIL KIM <dndlf_93@torpedo.co.kr>
2023-03-07 17:50:35 +0900

    카카오 로그인시 회원 가입 되어있는지 확인하는 코드 추가

--- server.js
+++ server.js
@@ -53,25 +53,64 @@ app.get("/auth/kakao/callback", async (req, res) => {
       },
     });
     const user = userResponse.data;
-    const session = neo4jDriver.session();
-    await session.run(
-      "MERGE (user:User {id: $id}) " +
-        "SET user.name = $name, user.email = $email " +
-        "RETURN user",
-      {
-        id: user.id,
-        name: user.properties.nickname,
-        email: user.kakao_account.email,
-        gender: user.kakao_account.gender,
-      }
-    );
-    session.close();
-    res.send(user);
+    if (await checkUserExistence(user.id)) {
+      console.log("들어옴?");
+      const session = neo4jDriver.session();
+      await session.run(
+        "MERGE (user:User {id: $id}) " +
+          "SET user.name = $name, user.email = $email, user.gender = $gender " +
+          "RETURN user",
+        {
+          id: user.id,
+          name: user.properties.nickname,
+          email: user.kakao_account.email,
+          gender: user.kakao_account.gender,
+        }
+      );
+      session.close();
+      res.send(user);
+    } else {
+      checkTokenInfo(accessToken);
+      res.status(200).json({ accessToken, user });
+    }
   } catch (err) {
     console.error(err);
     res.status(500).send("Server error");
   }
 });
+
+async function checkUserExistence(id) {
+  const session = neo4jDriver.session();
+  try {
+    const result = await session.run("MATCH (u:User {id: $id}) RETURN u", {
+      id,
+    });
+    if (result.records.length > 0) {
+      console.log("이미 가입되어있는 회원입니다");
+      return false;
+    } else {
+      console.log("회원 가입진행합니다");
+      return true;
+    }
+  } catch (err) {
+    console.error(err);
+    throw err;
+  } finally {
+    session.close();
+  }
+}
+
+async function checkTokenInfo(accessToken) {
+  const accessTokenInfo = await axios.get(
+    "https://kapi.kakao.com/v1/user/access_token_info",
+    {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    }
+  );
+  console.log(accessTokenInfo.data);
+}
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
